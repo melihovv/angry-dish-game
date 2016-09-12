2a813eeb6514a40890cd6b26e6eb7d942560fc6c
Александр Петренко <gafk555@gmail.com>
2016-09-12 18:53:32 +0300

    A class Field now has a set of FieldObjects and basic functionality to work with it. Also a functionality of listening field objects adding and deleting was implemented.

---
+++ src/main/java/melihovv/PetriDish/events/FieldObjectListener.java
@@ -0,0 +1,12 @@
+package melihovv.PetriDish.events;
+
+import melihovv.PetriDish.fieldObjects.FieldObject;
+
+/**
+ * A class represents listener interface for adding/deleting objects to/from game field.
+ */
+public interface FieldObjectListener {
+
+    void fieldObjectAdded( FieldObject fieldObject);
+    void fieldObjectDeleted( FieldObject fieldObject);
+}
--- src/main/java/melihovv/PetriDish/main/Field.java
+++ src/main/java/melihovv/PetriDish/main/Field.java
@@ -1,5 +1,11 @@
 package melihovv.PetriDish.main;
 
+import melihovv.PetriDish.events.FieldObjectListener;
+import melihovv.PetriDish.fieldObjects.FieldObject;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 /**
  * A class represents game field.
  * Singleton class.
@@ -10,6 +16,21 @@ public class Field {
     private final static int FIELD_HEIGH = 3750;
 
     private static Field _instance;
+    private ArrayList< FieldObject > _fieldObjects = new ArrayList<>();
+    private ArrayList< FieldObjectListener > _fieldObjectListeners = new ArrayList<>();
+
+    public void addFieldObject( FieldObject object, Point position ) {
+
+        object.setPosition( position );
+        _fieldObjects.add( object );
+        fireObjectAdded( object );
+    }
+
+    public void removeFieldObject( FieldObject object ) {
+
+        _fieldObjects.remove( object );
+        fireObjectDeleted( object );
+    }
 
     public static Field getInstance() {
 
@@ -29,5 +50,36 @@ public class Field {
     public static int getFieldHeigh() {
 
         return FIELD_HEIGH;
+    }
+
+    public ArrayList< FieldObject > getFieldObjects() {
+
+        return _fieldObjects;
+    }
+
+    public void addObjectListener(FieldObjectListener objectListener) {
+
+        _fieldObjectListeners.add( objectListener );
+    }
+
+    public void deleteObjectListener(FieldObjectListener objectListener) {
+
+        _fieldObjectListeners.remove( objectListener );
+    }
+
+    private void fireObjectAdded( FieldObject fieldObject ) {
+
+        for( FieldObjectListener objectListener : _fieldObjectListeners) {
+
+            objectListener.fieldObjectAdded( fieldObject );
+        }
+    }
+
+    private void fireObjectDeleted(FieldObject fieldObject) {
+
+        for( FieldObjectListener objectListener : _fieldObjectListeners ) {
+
+            objectListener.fieldObjectDeleted( fieldObject );
+        }
     }
 }
