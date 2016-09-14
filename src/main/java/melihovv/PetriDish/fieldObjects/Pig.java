3286172e70592b2785acb0fa5de0edced954054d
Александр Петренко <gafk555@gmail.com>
2016-09-14 20:08:37 +0300

    Added class Pig which represents a game object that can be eaten by a main hero.

---
+++ src/main/java/melihovv/PetriDish/fieldObjects/Pig.java
@@ -0,0 +1,18 @@
+package melihovv.PetriDish.fieldObjects;
+
+import melihovv.PetriDish.factories.GeneralFactory;
+
+/**
+ * A class represents game object which player can eat.
+ * Extends InactiveFieldObject.
+ */
+public class Pig extends InactiveFieldObject{
+
+    private final static int DEFAULT_SIZE = 64;
+
+    public Pig( GeneralFactory generalFactory ) {
+
+        super( generalFactory );
+        setSize( DEFAULT_SIZE );
+    }
+}
