367bc1f56456b70312dfa972a9f84bad44574b09
Александр Петренко <gafk555@gmail.com>
2016-09-14 20:36:15 +0300

    Added pigs to a game field.

--- src/main/java/melihovv/PetriDish/main/GameModel.java
+++ src/main/java/melihovv/PetriDish/main/GameModel.java
@@ -4,7 +4,7 @@ import melihovv.PetriDish.controllers.FieldObjectController;
 import melihovv.PetriDish.factories.FieldObjectsFactory;
 import melihovv.PetriDish.factories.GeneralFactory;
 import melihovv.PetriDish.fieldObjects.Bird;
-import melihovv.PetriDish.fieldObjects.FieldObject;
+import melihovv.PetriDish.fieldObjects.Pig;
 
 import java.awt.*;
 
@@ -13,6 +13,7 @@ import java.awt.*;
  */
 public class GameModel {
 
+    private final static int PIGS_COUNT = 50;
     private final static int DEFAULT_PLAYER_X_POSITION = 2500;
     private final static int DEFAULT_PLAYER_Y_POSITION = 1875;
 
@@ -33,13 +34,21 @@ public class GameModel {
 
     public void startGame() {
 
+        FieldObjectsFactory factory = new FieldObjectsFactory();
+
         /* Creating main player and its controller */
-        _player = ( Bird ) new FieldObjectsFactory().createFieldObject( "Bird",_generalFactory );
+        _player = ( Bird ) factory.createFieldObject( "Bird",_generalFactory );
         _playerController = _generalFactory.createPlayerController();
         _player.setController( _playerController );
-
-
+        
         Field.getInstance().addFieldObject( _player, new Point( DEFAULT_PLAYER_X_POSITION, DEFAULT_PLAYER_Y_POSITION ) );
+
+        /* Creating pigs */
+        for(int i = 0; i < PIGS_COUNT;++i) {
+
+            Pig pig = ( Pig ) factory.createFieldObject( "Pig",_generalFactory );
+            Field.getInstance().addFieldObjectToRandomPosition( pig );
+        }
     }
 
     public Bird getPlayer() {
